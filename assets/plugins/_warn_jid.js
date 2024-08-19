05f5ddeccdbfa5e240fc60c717f0351797b6171f
Ajsal sparky <130032226+A-J-S-A-L-S-P-A-R-K-Y@users.noreply.github.com>
2024-08-19 20:04:19 +0530

    Update and rename user.js to _warn_jid.js

---
+++ assets/plugins/_warn_jid.js
@@ -0,0 +1,105 @@
+const { command, isAdmin, parsedJid } = require("../../lib");
+const { exec } = require("child_process");
+const { PausedChats, WarnDB } = require("../database");
+const { WARN_COUNT } = require("../../config");
+const { secondsToDHMS } = require("../../lib/functions");
+const { saveWarn, resetWarn } = WarnDB;
+
+
+command(
+  {
+    pattern: "jid",
+    fromMe: true,
+    desc: "Give jid of chat/user",
+    type: "user",
+  },
+  async (message, match) => {
+    return await message.sendMessage(
+      message.jid,
+      message.mention[0] || message.reply_message.jid || message.jid
+    );
+  }
+);
+
+command(
+  {
+    pattern: "dlt",
+    fromMe: true,
+    desc: "deletes a message",
+    type: "user",
+  },
+  async (message, match, m, client) => {
+    if (message.isGroup) {
+      client.sendMessage(message.jid, { delete: message.reply_message.key });
+    }
+  }
+);
+
+command(
+  {
+    pattern: "warn",
+    fromMe: true,
+    desc: "Warn a user",
+  },
+  async (message, match) => {
+    const userId = message.mention[0] || message.reply_message.jid;
+    if (!userId) return message.reply("_Mention or reply to someone_");
+    let reason = message?.reply_message.text || match;
+    reason = reason.replace(/@(\d+)/, "");
+    reason = reason ? reason.length <= 1 : "Reason not Provided";
+
+    const warnInfo = await saveWarn(userId, reason);
+    let userWarnCount = warnInfo ? warnInfo.warnCount : 0;
+    userWarnCount++;
+    await message.reply(
+      `_User @${
+        userId.split("@")[0]
+      } warned._ \n_Warn Count: ${userWarnCount}._ \n_Reason: ${reason}_`,
+      { mentions: [userId] }
+    );
+    if (userWarnCount > WARN_COUNT) {
+      const jid = parsedJid(userId);
+      await message.sendMessage(
+        message.jid,
+        "Warn limit exceeded kicking user"
+      );
+      return await message.client.groupParticipantsUpdate(
+        message.jid,
+        jid,
+        "remove"
+      );
+    }
+    return;
+  }
+);
+
+command(
+  {
+    pattern: "resetwarn",
+    fromMe: true,
+    desc: "Reset warnings for a user",
+  },
+  async (message) => {
+    const userId = message.mention[0] || message.reply_message.jid;
+    if (!userId) return message.reply("_Mention or reply to someone_");
+    await resetWarn(userId);
+    return await message.reply(
+      `_Warnings for @${userId.split("@")[0]} reset_`,
+      {
+        mentions: [userId],
+      }
+    );
+  }
+);
+
+command(
+  {
+    pattern: "uptime",
+    fromMe: true,
+    desc: "Check uptime of bot",
+    type: "user",
+  },
+  async (message, match) => {
+    message.reply(`*Uptime:* ${secondsToDHMS(process.uptime())}`);
+  }
+);
