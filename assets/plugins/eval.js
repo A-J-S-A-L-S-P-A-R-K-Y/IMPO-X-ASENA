400f988121f43233db5c600271fd597512344d8e
Ajsal sparky <130032226+A-J-S-A-L-S-P-A-R-K-Y@users.noreply.github.com>
2024-08-19 08:16:22 +0530

    Update eval.js

--- assets/plugins/eval.js
+++ assets/plugins/eval.js
@@ -1,20 +1,24 @@
-/*const { command, getJson,isAdmin } = require("../../lib");
-const util = require("util");
-command(
-  { pattern: "> ?(.*)", fromMe: true, desc: "", type: "random" ,dontAddCommandList:true},
-  async (message, match,ms) => {
-    m = message;
-    conn = message.client;
-    const sock = message.client
-    try {
-      let evaled = await eval(match);
-      if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
-      await message.sendMessage(evaled);
-    } catch (err) {
-      await message.sendMessage(util.format(err));
-    }
-  }
-);
+const {
+    command,
+  } = require("../../lib/");
+  const util = require("util");
 
-/// © AJSAL SPARKY✅️
-*/
+  command(
+    {
+      on: "text",
+      fromMe: true,
+      desc: "eval",
+      type: "user",
+    },
+    async (message, match) => {
+        if (match.startsWith(">")) {
+			try {
+				let evaled = await eval(`(async () => { ${match.replace(">", "")} })()`);
+				if (typeof evaled !== "string") evaled = util.inspect(evaled);
+				await message.reply(`${evaled}`)
+			} catch (err) {
+				await message.reply(`${util.format(err)}`);
+			}
+		}
+    }
+  );
