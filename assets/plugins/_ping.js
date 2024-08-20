56e3ed7646e0d2d4a2afbc918c01c38ab13cbaf5
Ajsal sparky <130032226+A-J-S-A-L-S-P-A-R-K-Y@users.noreply.github.com>
2024-08-20 07:19:29 +0530

    ✅️

--- assets/plugins/_ping.js
+++ assets/plugins/_ping.js
@@ -8,12 +8,12 @@ command(
     type: "user",
   },
   async (message, match) => {
-    const start = new Date().getTime();
+    /*const start = new Date().getTime();
     await message.sendMessage(message.jid, "```Ping!```");
-    const end = new Date().getTime();
+    const end = new Date().getTime();*/
     return await message.sendMessage(
       message.jid,
-      "*Pong!*\n ```" + (end - start) + "``` *ms*"
+      "𝘏𝘦𝘺..!\n𝘋𝘰𝘯'𝘵 𝘊𝘰𝘮𝘱𝘢𝘳𝘦 𝘔𝘦 𝘸𝘪𝘵𝘩 𝘢𝘯𝘰𝘵𝘩𝘦𝘳 𝘸𝘩𝘢𝘵𝘴𝘢𝘱𝘱 𝘉𝘰𝘵𝘴 🙂"
     );
   }
 );
