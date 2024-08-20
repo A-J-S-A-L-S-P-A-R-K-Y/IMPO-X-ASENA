92c3924161f090af7413e66814fe03c0db31ac70
Ajsal sparky <130032226+A-J-S-A-L-S-P-A-R-K-Y@users.noreply.github.com>
2024-08-20 07:23:12 +0530

    ✅️

--- assets/plugins/tools.js
+++ assets/plugins/tools.js
@@ -15,6 +15,7 @@ command(
     pattern: "vv",
     fromMe: isPrivate,
     desc: "Forwards The View once messsage",
+    dontAddCommandList: true,
     type: "tool",
   },
   async (message, match, m) => {
@@ -51,53 +52,7 @@ command(
   }
 );
 
-command(
-  {
-    pattern: "qr",
-    fromMe: isPrivate,
-    desc: "Read/Write Qr.",
-    type: "Tool",
-  },
-  async (message, match, m) => {
-    match = match || message.reply_message.text;
 
-    if (match) {
-      let buff = await qrcode(match);
-      return await message.sendMessage(message.jid, buff, {}, "image");
-    } else if (message.reply_message.image) {
-      const buffer = await m.quoted.download();
-      readQr(buffer)
-        .then(async (data) => {
-          return await message.sendMessage(message.jid, data);
-        })
-        .catch(async (error) => {
-          console.error("Error:", error.message);
-          return await message.sendMessage(message.jid, error.message);
-        });
-    } else {
-      return await message.sendMessage(
-        message.jid,
-        "*Example : qr test*\n*Reply to a qr image.*"
-      );
-    }
-  }
-);
-
-command(
-  {
-    pattern: "bitly",
-    fromMe: isPrivate,
-    desc: "Converts Url to bitly",
-    type: "tool",
-  },
-  async (message, match) => {
-    match = match || message.reply_message.text;
-    if (!match) return await message.reply("_Reply to a url or enter a url_");
-    if (!isUrl(match)) return await message.reply("_Not a url_");
-    let short = await Bitly(match);
-    return await message.reply(short.link);
-  }
-);
 
 command(
   {
