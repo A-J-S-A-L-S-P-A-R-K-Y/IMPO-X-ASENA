f20281b53a2d9d291a0bb0c1fcb8da2b16d11fc5
Ajsal sparky <130032226+A-J-S-A-L-S-P-A-R-K-Y@users.noreply.github.com>
2024-08-20 09:32:18 +0530

    Update tools.js

--- assets/plugins/tools.js
+++ assets/plugins/tools.js
@@ -54,34 +54,3 @@ command(
 
 
 
-command(
-  {
-    pattern: "lyric",
-    fromMe: isPrivate,
-    desc: "Searches for lyrics based on the format: song;artist",
-    type: "tools",
-  },
-  async (message, match) => {
-    const [song, artist] = match.split(";").map((item) => item.trim());
-    if (!song || !artist) {
-      await message.reply("Search with this format: \n\t_lyric song;artist_");
-    } else {
-      try {
-        const data = await getLyrics(song, artist);
-        if (data) {
-          return await message.reply(
-            `*Artist:* ${data.artist_name}\n*Song:* ${
-              data.song
-            }\n*Lyrics:*\n${data.lyrics.trim()}`
-          );
-        } else {
-          return await message.reply(
-            "No lyrics found for this song by this artist."
-          );
-        }
-      } catch (error) {
-        return await message.reply("An error occurred while fetching lyrics.");
-      }
-    }
-  }
-);
