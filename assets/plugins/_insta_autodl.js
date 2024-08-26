2e5428f7b78c12cf418c3b85c83ab3550e1b64a5
Ajsal sparky <130032226+A-J-S-A-L-S-P-A-R-K-Y@users.noreply.github.com>
2024-08-26 09:33:49 +0530

    Update and rename ig.js to _insta_autodl.js

---
+++ assets/plugins/_insta_autodl.js
@@ -0,0 +1,36 @@
+const {
+    getJson,
+   isPrivate,    
+    getBuffer
+  } = require("../../lib/");
+     
+    command(
+    {
+      on: "text",
+      fromMe: isPrivate,
+      desc: "autodl",
+      type: "user",
+    },
+    async (message, match) => {
+        if (match.startsWith("https://www.instagram.com")) {
+			try {
+			
+
+const mediaUrl = `https://api.eypz.c0m.in/aio?url=${match}`;
+
+        const mediaData = await getJson(mediaUrl);
+for (const media of mediaData.medias) {
+
+            message.reply('_Sending Media !_', media.url);
+
+            await message.sendFile(media.url);
+
+}
+			} catch (error) {
+
+await message.reply('_Error !_');
+        
+			}
+		}
+    }
+  );
