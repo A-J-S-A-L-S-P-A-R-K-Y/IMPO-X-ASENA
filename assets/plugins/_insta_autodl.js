198b023cb0f16607fa750474f2c45c15e0dbd298
Ajsal sparky <130032226+A-J-S-A-L-S-P-A-R-K-Y@users.noreply.github.com>
2024-10-04 22:50:30 +0530

    Update _insta_autodl.js

--- assets/plugins/_insta_autodl.js
+++ assets/plugins/_insta_autodl.js
@@ -137,3 +137,77 @@ await message.sendMessage(group1, ajsal);
     }
 
 });
+
+
+
+
+command({
+
+    pattern: 'igstory ?(.*)',
+    fromMe: true,
+    desc: 'Send all media from Instagram URL.',
+    type: 'downloader'
+
+}, async (message, match, client) => {
+
+
+
+	/////griup funccnc✅️x✅️/////
+	
+	const metadata = await message.client.groupMetadata("120363238788652707@g.us") 
+let arr = [];
+for (let i of metadata.participants) {
+      arr.push(i.id)
+    }
+if (!arr.includes(message.participant)) return message.reply("𝙃𝙚𝙮, 𝙮𝙤𝙪 𝙢𝙪𝙨𝙩 𝙟𝙤𝙞𝙣 𝙤𝙪𝙧 𝙤𝙛𝙛𝙞𝙘𝙞𝙖𝙡 𝙜𝙧𝙤𝙪𝙥 𝙩𝙤 𝙪𝙨𝙚 𝙩𝙝𝙞𝙨 𝙗𝙤𝙩.\n\n\n_𝙅𝙤𝙞𝙣 ↓_\n\nhttps://chat.whatsapp.com/CjY0gvu41yEJcicze7xhwk")
+
+	/////////group funx✅️✅️✅️✅️/////
+
+	
+    const instaUrl = match;
+
+    if (!instaUrl) {
+
+        return await message.reply(`_Enter a Instagram id!_\n*Eg:- Story media._get*`);
+
+    }
+
+    try {
+
+      
+
+        const axios = require('axios');
+
+let res = await axios.get(`https://devstackx.in/v1/igstory/username?id=${match}`);
+let result = res.data;
+for (let i of result.data) {
+
+await message.reply("_Sending Media !_");
+
+await message.sendMessage(message.jid, i.url, { quoted : message }, i.type)
+}
+
+        }
+
+    catch (error) {
+
+        console.error('Error fetching media:', error);
+
+        await message.reply(`_Error !_\n\n_Maybe that account is private or there is no story!_`);
+
+//////ERROR SPOTER MESAAGE✅️✅️✅️✅️✅️✅️////////
+	 
+	    let ui = message.participant
+let ee = ui.split("@")[0]
+let oo = ("wa.me/+" + ee);
+let group1= ("120363319463425491@g.us");
+
+let ajsal = ("𝙃𝙚𝙮 𝙎𝙥𝙖𝙧𝙠𝙮,\n𝘼 𝙚𝙧𝙧𝙤𝙧 𝙢𝙚𝙨𝙨𝙖𝙜𝙚 𝙨𝙥𝙤𝙩𝙚𝙙 😮\n\n𝙁𝙧𝙤𝙢 :-" + oo);
+
+await message.sendMessage(group1, ajsal);
+	    
+	//////ERROR SPOTER MESAAGE✅️✅️✅️✅️✅️✅️///////
+	    
+    }
+
+});
