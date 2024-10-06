cff20e2eca8de4dda2b29d2de29d6aae97392ab0
Ajsal sparky <130032226+A-J-S-A-L-S-P-A-R-K-Y@users.noreply.github.com>
2024-10-06 19:40:49 +0530

    Update _ping.js

--- assets/plugins/_ping.js
+++ assets/plugins/_ping.js
@@ -1,4 +1,4 @@
-const { fromBuffer, mimeTypes } = require("file-type");
+/*const { fromBuffer, mimeTypes } = require("file-type");
 const { command, isPublic } = require("../../lib/");
 command(
   {
@@ -8,9 +8,9 @@ command(
     type: "user",
   },
   async (message, match) => {
-    /*const start = new Date().getTime();
+    const start = new Date().getTime();
     await message.sendMessage(message.jid, "```Ping!```");
-    const end = new Date().getTime();*/
+    const end = new Date().getTime();
     return await message.sendMessage(
       message.jid,
       "𝘏𝘦𝘺..!\n𝘋𝘰𝘯'𝘵 𝘊𝘰𝘮𝘱𝘢𝘳𝘦 𝘔𝘦 𝘸𝘪𝘵𝘩 𝘢𝘯𝘰𝘵𝘩𝘦𝘳 𝘸𝘩𝘢𝘵𝘴𝘢𝘱𝘱 𝘉𝘰𝘵𝘴 🙂"
@@ -26,9 +26,9 @@ command(
     type: "user",
   },
   async (message, match) => {
-    /*const start = new Date().getTime();
+    const start = new Date().getTime();
     await message.sendMessage(message.jid, "```Ping!```");
-    const end = new Date().getTime();*/
+    const end = new Date().getTime();
     return await message.sendMessage(
       message.jid,
       "ī.am 𝙎𝙩𝙞𝙡𝙡 𝙖𝙡𝙞𝙫𝙚\n_Paste url for download_"
@@ -36,4 +36,4 @@ command(
   }
 );
 
-
+*/
