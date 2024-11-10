84eb5eb199c440a104290788388365e986b550c6
Ajsal sparky <130032226+A-J-S-A-L-S-P-A-R-K-Y@users.noreply.github.com>
2024-11-10 09:36:16 +0530

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-
-http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('bot\'s running!\n');
-}).listen(7070, () => {
-  console.log('Server listening on port 8000');
-});
